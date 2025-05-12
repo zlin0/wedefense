7bc6ea70190fae248e92c2fdd8023a7f720af936
zlin0 <lzhan268@jh.edu>
2025-05-12 17:13:52 -0400

    [ssl] clean ssl code

--- wedefense/models/ssl_backend/MHFA.py
+++ wedefense/models/ssl_backend/MHFA.py
@@ -1,3 +1,26 @@
+# Copyright (c) 2025 Junyi Peng (pengjy@fit.vut.cz)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+MHFA as the backend for SSL models.
+
+From the paper: An attention-based backend allowing efficient fine-tuning 
+                of transformer models for speaker verification
+Author: Junyi Peng, Oldrich Plchot, Themos Stafylakis, Ladislav Mosner, 
+        Lukas Burget, Jan Cernocky
+Link: https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=10022775
+"""
+
 import math
 import torch
 import torch.nn as nn

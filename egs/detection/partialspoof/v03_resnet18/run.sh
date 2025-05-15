3bbd09b84a06112fad1b5895a2421d1e1abc0efc
Lin Zhang <lzhan268@jh.edu>
2025-05-15 15:18:09 -0400

    [fix] stage le/ge in run.sh

--- egs/detection/partialspoof/v03_resnet18/run.sh
+++ egs/detection/partialspoof/v03_resnet18/run.sh
@@ -100,7 +100,7 @@ model_path=$avg_model
 #######################################################################################
 # Stage 4. Averaging the model, and extract embeddings
 #######################################################################################
-if [ ${stage} -ge 4 ] && [ ${stop_stage} -le 4 ]; then
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
   echo "Do model average ..."
   python wedefense/bin/average_model.py \
     --dst_model $avg_model \

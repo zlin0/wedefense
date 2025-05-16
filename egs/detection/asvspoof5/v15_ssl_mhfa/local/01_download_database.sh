c9d33a3932ad759cc04f409be21fa4e66844aef3
Peng Junyi <pengjy@merlin.fit.vutbr.cz>
2025-05-16 09:12:49 +0200

    fix ASVspoof5 download script

--- egs/detection/asvspoof5/v15_ssl_mhfa/local/01_download_database.sh
+++ egs/detection/asvspoof5/v15_ssl_mhfa/local/01_download_database.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -x
+# set -x
 ASVspoof5_dir=$1
 dset=$2
+RED='\033[0;31m'
 
 if [ "$dset" = "E"  ]; then
     FILE_NAMEs="a b c d e f g h i j"
@@ -11,17 +12,21 @@ else
     FILE_NAMEs="a b c d e"
 fi
 
+mkdir -p "${ASVspoof5_dir}"
+
 #for dset in T D E; do
     for name in ${FILE_NAMEs}; do    
         fname=flac_${dset}_a${name}.tar
         if [ ! -d ${ASVspoof5_dir}/flac_${dset} ]; then
-            link="https://zenodo.org/records/14498691/files/${fname}?dowbload=1"
+            link="https://zenodo.org/records/14498691/files/${fname}?download=1"
             echo -e "${RED}Downloading ${fname}"
             echo ${link}
-            wget -q --show-progress -c ${link} -O  $fname
-            tar -xvf ${fname}
-            rm ${name}.tar
+            wget -q --show-progress -c "${link}" -O "${ASVspoof5_dir}/${fname}"
+            tar -xf "${ASVspoof5_dir}/${fname}" -C "${ASVspoof5_dir}"
+            rm "${ASVspoof5_dir}/${fname}"
+        else
+            echo "Directory ${ASVspoof5_dir}/flac_${dset}_a${name} already exists, skipping..."
     fi
     done
 #done
-echo -e "${RED} We have ASVspoof5 database on "${ASVspoof5_dir}' now'
+echo -e "${RED} We have ASVspoof5 database on ${ASVspoof5_dir} now"

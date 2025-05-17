87407f711035e946c7fafd0174950eb806042f29
WangXin <wangxin@nii.ac.jp>
2025-05-18 08:37:54 +0900

    [feature/block_shuffle] update conf yaml file; upgrade elem.storage() to elem._typed_storage() to suppress warning

--- egs/detection/partialspoof/v03_resnet18/run.sh
+++ egs/detection/partialspoof/v03_resnet18/run.sh
@@ -14,10 +14,10 @@ data=data/partialspoof # data folder
 #data_type="shard"  # shard/raw
 data_type="raw"  # shard/raw
 
-config=conf/resnet_wholeutt_noaug_nosampler.yaml 
-exp_dir=exp/ResNet18-TSTP-emb256-fbank80-wholeutt_nosampler-aug0-spFalse-saFalse-Softmax-SGD-epoch100
 config=conf/resnet.yaml #wespeaker version 
 exp_dir=exp/ResNet18-TSTP-emb256-fbank80-frms400-aug0-spFalse-saFalse-Softmax-SGD-epoch100
+config=conf/resnet_wholeutt_noaug_nosampler.yaml 
+exp_dir=exp/ResNet18-TSTP-emb256-fbank80-wholeutt_nosampler-aug0-spFalse-saFalse-Softmax-SGD-epoch100
 
 gpus="[0]"
 num_avg=10 # how many models you want to average

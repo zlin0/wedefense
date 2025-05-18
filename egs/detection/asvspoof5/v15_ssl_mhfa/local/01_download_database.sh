9056bb43a66b83a176e344973c62197a3448d4a1
Lin Zhang <lzhan268@jh.edu>
2025-05-18 19:08:23 -0400

    [fix] add downloading license and readme for asvspoof5

--- egs/detection/asvspoof5/v15_ssl_mhfa/local/01_download_database.sh
+++ egs/detection/asvspoof5/v15_ssl_mhfa/local/01_download_database.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # set -x
 ASVspoof5_dir=$1
-dset=$2
 RED='\033[0;31m'
 
 if [ "$dset" = "E"  ]; then
@@ -14,7 +13,7 @@ fi
 
 mkdir -p "${ASVspoof5_dir}"
 
-#for dset in T D E; do
+for dset in T D E; do
     for name in ${FILE_NAMEs}; do    
         fname=flac_${dset}_a${name}.tar
         # if [ ! -d ${ASVspoof5_dir}/flac_${dset} ]; then
@@ -28,5 +27,21 @@ mkdir -p "${ASVspoof5_dir}"
             # echo "Directory ${ASVspoof5_dir}/flac_${dset}_a${name} already exists, skipping..."
     # fi
     done
-#done
+done
+
+fname=ASVspoof5_protocols.tar.gz
+link="https://zenodo.org/records/14498691/files/${fname}?download=1"
+echo -e "${RED}Downloading ${fname}"
+wget -q --show-progress -c ${link} -O "${ASVspoof5_dir}/${fname}"
+tar -xf "${ASVspoof5_dir}/${fname}" -C "${ASVspoof5_dir}"
+rm "${ASVspoof5_dir}/${fname}"
+
+OTH_files="LICENSE.txt README.txt"
+for fname in $OTH_files; do
+    link="https://zenodo.org/records/14498691/files/${fname}?download=1"
+    wget -q --show-progress -c "${link}" -O "${ASVspoof5_dir}/${fname}"
+    echo -e "${RED}Downloading ${fname}"
+done	
+
+
 echo -e "${RED} We have ASVspoof5 database on ${ASVspoof5_dir} now"

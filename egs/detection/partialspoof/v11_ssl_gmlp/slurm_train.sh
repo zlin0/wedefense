7aed4859821864e925cc885b4f3629d985041c81
Lin Zhang <lzhan268@jh.edu>
2025-05-20 01:06:13 -0400

    [gmlp] update slurm script

--- egs/detection/partialspoof/v11_ssl_gmlp/slurm_train.sh
+++ egs/detection/partialspoof/v11_ssl_gmlp/slurm_train.sh
@@ -4,12 +4,12 @@
 #SBATCH --nodes=1  #number of nodes requested
 #SBATCH --gpus=1
 #SBATCH --partition=gpu   #queue
-#SBATCH --partition=gpu-a100   # the a100 partition, for other gpu, use partition=gpu
-#SBATCH --account=a100acct #we need this only for the a100 gpus
+##SBATCH --partition=gpu-a100   # the a100 partition, for other gpu, use partition=gpu
+##SBATCH --account=a100acct #we need this only for the a100 gpus
 #SBATCH --mail-user="lzhan268@jh.edu"  #email for reporting
 #SBATCH --mail-type=END,FAIL  #report types
 #SBATCH --output=./logs/slurm-%A_%a.out
-#SBATCH --array=1-1
+#SBATCH --array=2-2
 
 #unset PYTHONPATH
 #unset PYTHONHOME
@@ -22,7 +22,8 @@ conda activate wedefense_pip
 echo "[$(date)] Starting Job ID: $SLURM_JOB_ID" > logs/debug_$SLURM_JOB_ID.txt
 
 CONFIGs=(singlereso-utt_gmlp_wavlm-large_frozen
-	 singlereso-utt_gmlp_xlsr53_ft)
+	 singlereso-utt_gmlp_xlsr53_ft
+	 singlereso-utt_gmlp_xlsr53_frozen)
 config_name=${CONFIGs[$SLURM_ARRAY_TASK_ID]} 
 
 

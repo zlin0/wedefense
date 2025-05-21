fa25a8af969153305d1693bc8b5211b607859ada
Lin Zhang <lzhan268@jh.edu>
2025-05-20 23:26:35 -0400

    [feature/lfcc] fix bug in dimention and excutor.py

--- wedefense/utils/executor.py
+++ wedefense/utils/executor.py
@@ -36,7 +36,7 @@ def run_epoch(dataloader, epoch_iter, model, criterion, optimizer, scheduler,
         utts = batch['key']
         targets = batch['label']
         targets = targets.long().to(device)  # (B)
-        if frontend_type == 'fbank':
+        if frontend_type == 'fbank' or 'lfcc' in frontend_type:
             features = batch['feat']  # (B,T,F)
             features = features.float().to(device)
         else:  # 's3prl'

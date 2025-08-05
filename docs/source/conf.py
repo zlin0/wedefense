3eea82c70faddb28c742235199d877d3e98b67b9
Lin Zhang <72589459+zlin0@users.noreply.github.com>
2025-08-05 02:32:28 -0400

    [docs] Initial creation of documentation (#75)
    * [doc] add recipe to create conda env.
    
    * [doc] add recipe to create conda env.
    
    * [doc] fix typo
    
    * [doc] initiall documents
    
    * [lint] add more hooks for pre-commit
    
    * [doc] remove unused file
    
    * [doc] update readme

---
+++ docs/source/conf.py
@@ -0,0 +1,81 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'WeDefense'
+copyright = '2025, WeDefense'
+author = 'WeDefense'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.autosummary',
+    'sphinx_copybutton',
+    'sphinx_design',
+    'sphinx_markdown_tables',
+    'myst_parser',
+]
+
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# Intersphinx mapping:
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+    'torch': ('https://pytorch.org/docs/master/', None),
+    'torchaudio': ('https://pytorch.org/audio/stable/', None),
+}
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+# See https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
+# for rtd theme options
+html_theme_options = {
+    'logo_only': True,
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+}
+html_logo = '_static/wedefense_logo_h.png'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
---
+++ docs/source/faq.md
@@ -0,0 +1,8 @@
+# Frequently Asked Questions (FAQs)
+In this section, we collect issues reported by users and post the corresponding solutions.
+
+
+
+
+- For more help please open an issue on [GitHub](https://github.com/zlin0/wedefense/issues).
+---

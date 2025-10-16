eba4c055a2bb2e52f166d8ba67d3ab2f81dbec74
zlin0 <lzhan268@jh.edu>
2025-10-16 14:12:35 -0400

    [feat] add scripts for downloading and preparing MUSAN RIR datasets

---
+++ egs/other/musan_rir/local/prepare_data_musan_rir.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Copyright (c) 2022 Hongji Wang (jijijiang77@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+stage=3
+stop_stage=3
+data=data
+
+. tools/parse_options.sh || exit 1
+
+data=`realpath ${data}`
+download_dir=${data}/download_data
+rawdata_dir=${data}/raw_data
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+  echo "Download musan.tar.gz, rirs_noises.zip."
+  echo "This may take a long time. Thus we recommand you to download all archives above in your own way first."
+
+  ./local/download_data.sh --download_dir ${download_dir}
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+  echo "Decompress all archives ..."
+  echo "This could take some time ..."
+
+  for archive in musan.tar.gz rirs_noises.zip ; do
+    [ ! -f ${download_dir}/$archive ] && echo "Archive $archive not exists !!!" && exit 1
+  done
+  [ ! -d ${rawdata_dir} ] && mkdir -p ${rawdata_dir}
+
+  if [ ! -d ${rawdata_dir}/musan ]; then
+    tar -xzvf ${download_dir}/musan.tar.gz -C ${rawdata_dir}
+  fi
+
+  if [ ! -d ${rawdata_dir}/RIRS_NOISES ]; then
+    unzip ${download_dir}/rirs_noises.zip -d ${rawdata_dir}
+  fi
+
+  echo "Decompress success !!!"
+fi
+
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+  echo "Prepare wav.scp for each dataset ..."
+  export LC_ALL=C # kaldi config
+
+  mkdir -p ${data}/musan ${data}/rirs
+  # musan
+  find ${rawdata_dir}/musan -name "*.wav" | awk -F"/" '{print $(NF-2)"/"$(NF-1)"/"$NF,$0}' >${data}/musan/wav.scp
+  # rirs
+  find ${rawdata_dir}/RIRS_NOISES/simulated_rirs -name "*.wav" | awk -F"/" '{print $(NF-2)"/"$(NF-1)"/"$NF,$0}' >${data}/rirs/wav.scp
+
+  echo "Success !!!"
+fi
